c9d6b8f22482eb065b34108bd9d7a242fb12c27b
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-07-28 23:36:23 -0300

    Mapeamento FluentNHibernate para a consulta.

---
+++ ConsultorioMedico.Backend/ConsultorioMedico.Infrastructure/Mappings/ConsultaMap.cs
@@ -0,0 +1,33 @@
+﻿using FluentNHibernate.Mapping;
+using ConsultorioMedico.Dominio.Entities;
+
+namespace ConsultorioMedico.Infrastructure.Mappings
+{
+    public class ConsultaMap : ClassMap<Consulta>
+    {
+        public ConsultaMap()
+        {
+            Table("Consultas");
+
+            Id(x => x.Id).GeneratedBy.Sequence("consulta_sequence");
+
+            Map(x => x.DataHoraConsulta)
+                .Not.Nullable()
+                .CustomType("timestamp");
+
+            Map(x => x.Observacoes)
+                .Length(100)
+                .Nullable();
+
+            References(x => x.Paciente)
+                .Column("PacienteId")
+                .Not.Nullable()
+                .Cascade.None();
+
+            References(x => x.Medico)
+                .Column("MedicoId")
+                .Not.Nullable()
+                .Cascade.None();
+        }
+    }
+}
